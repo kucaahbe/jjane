68356c84e8e377b1f4dc47075264a553fa49c22f
kucaahbe <powta@ukr.net>
2009-12-04 20:08:06 +0200

    public files install script

--- lib/jjane.rb
+++ lib/jjane.rb
@@ -7,6 +7,18 @@ require 'jjane/action_controller_extensions'
   ActiveSupport::Dependencies.load_paths << path
   ActiveSupport::Dependencies.load_once_paths.delete(path)
 end
-
 ActionController::Base.append_view_path File.join(File.dirname(__FILE__), 'app/views')
+
 ActionView::Base.send :include, JJane::Helpers::CoreHelper
+
+# install nessesary files
+unless File.exists?(File.join(RAILS_ROOT,'public/jjane'))
+  puts '=> installing JJane public files...'
+  dest = File.join RAILS_ROOT, 'public', 'jjane'
+  FileUtils.mkdir dest
+
+  %w[ images javascripts stylesheets ].each do |dir|
+    source = File.join File.dirname(__FILE__), '..', 'public'
+    FileUtils.cp_r File.join(source, dir), dest
+  end
+end
