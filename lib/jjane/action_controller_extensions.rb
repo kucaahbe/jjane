06453ef0e1e275ce6d02c2d7730489526d17c4cc
kucaahbe <powta@ukr.net>
2009-12-06 00:39:55 +0200

    checkpoint,routes are not finished

--- lib/jjane/action_controller_extensions.rb
+++ lib/jjane/action_controller_extensions.rb
@@ -1,56 +1,60 @@
-class ActionController::Base
+module ActionController#:nodoc:
+  class Base#:doc:
 
-  filter_parameter_logging :password, :content
+    filter_parameter_logging :password, :content
 
-  helper_method :log
+    helper_method :log
 
-  private
+    private
+    
+    include JJane::Helpers::UserSessionHelper
 
-  include JJane::Helpers::UserSessionHelper
-
-  def check_access
-    error_404 unless logged_in?('root') 
-  end
-
-  def error_404
-    flash[:notice] = "The requested URL #{request.request_uri} was not found on this server."; flash.discard
-    render '/site/error_404', :status => 404, :layout => 'application'
-  rescue
-    render_optional_error_file(404)
-  end
-
-  # ---------------------------
-  #        DRY methods
-  # ---------------------------
-  def get_model_name
-    self.class.to_s.gsub(/Controller/,'').singularize
-  end
-
-  def notice(word)
-    flash[:notice] = "#{get_model_name} was successfully #{word}."
-  end
-
-  def partial(view, params = nil)
-    render :partial => view, :locals => params
-  end
-
-  def find_model
-    get_model_name.constantize.find(params[:id])
-  end
-
-  # ---------------------------
-  #       DEBUG METHODS
-  # ---------------------------
-  def log(something,msg='')
-    logger.info "###########################################################"
-    if something.class==Array
-      logger.info "DEBUG::[#{something.class}]"
-      something.each_with_index do |s,i|
-	logger.info "       [#{i}][#{s.class}]"+s.inspect
-      end
-    else
-      logger.info "DEBUG::[#{something.class}]> "+something.inspect
+    def check_access
+      error_404 unless logged_in?('root') 
     end
-    logger.info msg
+
+    # shows ERROR 404
+    def error_404 #:doc:
+      flash[:notice] = "The requested URL #{request.request_uri} was not found on this server."; flash.discard
+      render '/site/error_404', :status => 404, :layout => 'application'
+    rescue
+      render_optional_error_file(404)
+    end
+
+    #--
+    #        DRY methods
+    #++
+    def get_model_name
+      self.class.to_s.gsub(/Controller/,'').singularize
+    end
+
+    def notice(word)
+      flash[:notice] = "#{get_model_name} was successfully #{word}."
+    end
+
+    def partial(view, params = nil)
+      render :partial => view, :locals => params
+    end
+
+    def find_model
+      get_model_name.constantize.find(params[:id])
+    end
+
+    #--
+    #       DEBUG METHODS
+    #++
+    # if you want to log something
+    def log(something,msg='')#:doc:
+      logger.info "###########################################################"
+      if something.class==Array
+	logger.info "DEBUG::[#{something.class}]"
+	something.each_with_index do |s,i|
+	  logger.info "       [#{i}][#{s.class}]"+s.inspect
+	end
+      else
+	logger.info "DEBUG::[#{something.class}]> "+something.inspect
+      end
+      logger.info msg
+    end
   end
 end
--- lib/jjane/routing_extensions.rb
+++ lib/jjane/routing_extensions.rb
@@ -1,20 +1,63 @@
 module JJane#:nodoc:
   module Routing #:nodoc:
+    # JJane adds routing [number here] extensions
+    # sample config/routes.rb file:
+    #  ActionController::Routing::Routes.draw do |map|
+    #    map.connect ':controller/:action/:id'
+    #    map.connect ':controller/:action/:id.:format'
+    #
+    #    map.jjane_admin 'administration'
+    #    map.jjane_root
+    #    map.jjane_connect
+    #  end
     module MapperExtensions
+      # RESTfull routes for nodes
+      def jjane_resources(node_name)#-- #FIXME don't work #++
+	namespace :pages do |pages|
+	  pages.resources node_name
+	end
+      end
+
+      #   map.welcome 'path_to_admin_login', :controller => :admin, :action => :welcome
+      def jjane_admin(path_to_admin_login='admin')
+	welcome path_to_admin_login, :controller => :admin, :action => :welcome
+      end
+
+      # it is the same as 
+      #   map.root :controller => :site, :action => :home_page
+      def jjane_root
+	root :controller => :site, :action => :home_page
+      end
+
       def jjane_connect
-	@set.add_route '*uri/:year/:month/:day', {
+	resources :pages, :except => [:show], :collection => { :sort => :put } do |page|
+	  page.resources :child, :controller => :pages, :only => [:new]
+	  #  page.resources :nodes,:except => [:index, :show]
+	end
+
+	resources :snippets, :except => [:show]
+
+	resources :users
+
+	login '__login__', { :controller => :admin, :action => :login }
+
+	logout '__logout__', { :controller => :admin, :action => :logout }
+
+	connect '*uri/:year/:month/:day', {
 	  :controller => 'site',
 	  :action     => 'find_by_day',
 	  :year       => /\d{4}/,
 	  :month      => /\d{1,2}/,
 	  :day        => /\d{1,2}/
 	}
-	@set.add_route '*uri/:id', {
+
+	connect '*uri/:id', {
 	  :controller => :site,
 	  :action => :node,
-	  :requirements => {:id => /\d+/}
+	  :requirements => { :id => /\d+/ }
 	}
-	@set.add_route '*uri', {
+
+	connect '*uri', {
 	  :controller => :site,
 	  :action => :page
 	}
@@ -24,35 +67,3 @@ module JJane#:nodoc:
 end
 
 ActionController::Routing::RouteSet::Mapper.send :include, JJane::Routing::MapperExtensions
-
-=begin
-ActionController::Routing::Routes.draw do |map|
-
-  # root route
-  map.root :controller => :site, :action => :home_page
-
-  # routes for filesystem
-  map.resources :attached_files do |m|
-    m.resource :directory, :controller => :attached_files, :only => :new
-    m.upload 'file/new', :controller => :attached_files, :action => :new_file
-  end
-
-#---------------------------------------ENGINE\/
-
-  # resource-oriented routes
-  map.resources :pages, :except => [:show], :collection => { :sort => :put } do |page|
-    page.resources :child, :controller => :pages, :only => [:new]
-    page.resources :nodes,:except => [:index, :show]
-    page.resources :articles
-  end
-  map.resources :users, :snippets
-
-  # named routes
-  map.welcome 'admin', :controller => :admin, :action => :welcome
-  map.login '__login__', :controller => :admin, :action => :login
-  map.logout '__logout__', :controller => :admin, :action => :logout
-
-
-
-end
-=end
