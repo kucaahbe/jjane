ffbcab67f6e0a4c57df274476c168d34e015e883
kucaahbe <powta@ukr.net>
2009-12-22 18:38:01 +0200

    added preview to nodes  [mozilla fix]

--- public/javascripts/jjane.js
+++ public/javascripts/jjane.js
@@ -1,14 +1,17 @@
 // для показа/скрытия превьюшек для nodes
 function PreviewNodeShowHide() {
   var p_node = $('preview_node');
+  var text_areas = $$('#preview_node textarea')
   if (p_node.hasClassName('collapsed'))
   {
     p_node.removeClassName('collapsed');
     p_node.addClassName('expanded');
+    text_areas.each(function(ta) { ta.show(); });
   }
   else
   {
     p_node.removeClassName('expanded');
     p_node.addClassName('collapsed');
+    text_areas.each(function(ta) { ta.hide(); });
   }
 }
