aa7432a530c073f599a8162ec6dac17bc4d8da59
kucaahbe <powta@ukr.net>
2009-12-25 13:50:30 +0200

    moeta editing works without warnings now

--- lib/jjane/models/j_jane_node.rb
+++ lib/jjane/models/j_jane_node.rb
@@ -3,25 +3,17 @@ class JJaneNode < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :page
-  has_one    :meta, :class_name => 'JJaneMeta', :foreign_key => :node_id, :dependent => :destroy
 #  belongs_to :attached_file, :foreign_key => :file_id
 
+  has_one    :meta,
+    :class_name => 'JJaneMeta',
+    :foreign_key => :node_id,
+    :dependent => :destroy
   accepts_nested_attributes_for :meta
-  attr_accessible :title, :content, :preview, :page_id, :user_id
+
+  attr_accessible :title, :content, :preview, :page_id, :user_id, :meta_attributes
 
   validates_presence_of :title, :page_id, :user_id
-
-  def initialize(attributes = nil)
-    logger.info '-----------'
-    super
-    logger.info self.inspect
-    if attributes && attributes[:meta]
-      self.build_meta(attributes[:meta])
-    else
-      self.build_meta
-    end
-    logger.info self.meta.inspect
-  end
 
   def url
     self.page.url+'/'+self.id.to_s
