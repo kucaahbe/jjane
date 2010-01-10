8474f5d5e5464cdd1028d349788f56f94c1d3ba4
kucaahbe <powta@ukr.net>
2010-01-10 04:07:48 +0200

    updated will_paginate plugin added ability to chenge link in pages

--- lib/jjane/rails_config.rb
+++ lib/jjane/rails_config.rb
@@ -1,8 +1,7 @@
 Rails.configuration.gem 'authlogic'
-Rails.configuration.gem 'mislav-will_paginate',
-                        :version => '~> 2.3.11',
-                        :lib => 'will_paginate',
-                        :source => 'http://gems.github.com'
+Rails.configuration.gem 'will_paginate',
+                        :version => '2.3.11',
+                        :source => 'http://gemcutter.org'
 Rails.configuration.gem "robinsp-sortable_element_for_nested_set",
                         :lib    => "sortable_element_for_nested_set",
                         :source => "http://gems.github.com"
