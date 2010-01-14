9c8971b1305c63844711633f48ba7f96a4136862
kucaahbe <powta@ukr.net>
2010-01-14 19:32:49 +0200

    fixing and adding features to navigation helpers 	modified:   lib/jjane/helpers/navigation.rb 	modified:   lib/jjane/views/shared/_ul_li_menu.html.erb

--- lib/jjane/helpers/navigation.rb
+++ lib/jjane/helpers/navigation.rb
@@ -3,55 +3,71 @@ module JJane
     module Navigation
 
       # Breadcrumbs
-      #   :include_self - if true then adds current page link
-      #   :separator - wich separator use,default: '&nbsp;&raquo;&nbsp;'
-      #   :before - what to write before breadcrumbs
-      #   :after - what to write after breadcrumbs
-      #   :include_node - if true then include node title
-      def breadcrumbs(args={})
+      #   :framing_tag (default=:div)
+      #   :include_self - if true then adds current page link(default=true)
+      #   :separator - wich separator use(default='&nbsp;&raquo;&nbsp;')
+      #   :before - what to write before breadcrumbs(default='&nbsp;&raquo;&nbsp;')
+      #   :after - what to write after breadcrumbs(default='')
+      #   :include_node - if true then include node title(default=false)
+      #   :before_node
+      #   :after_node
+      def breadcrumbs(options={})
 	default_options = {
+	  :framing_tag => :div,
 	  :include_self => true,
 	  :before => '',
 	  :after => '',
 	  :separator => '&nbsp;&raquo;&nbsp;',
-	  :include_node => false
+	  :include_node => false,
+	  :before_node => '',
+	  :after_node => ''
 	}
-	args = default_options.merge(args)
+	default_html_options = { :id => 'breadcrumbs' }
 
-	line = args[:before]
-	@page.ancestors.each do |page|
-	  line += link_to(page.menu, root_url+page.url) + args[:separator]
+	if options[:html]
+	  options[:html] = default_html_options.merge(options[:html])
+	else
+	  options[:html] = default_html_options
 	end
-	line += link_to(@page.menu,root_url+@page.url) if args[:include_self]
+	options = default_options.merge(options)
 
-	line += args[:separator] + link_to(@node.title,root_url+@node.url)  if defined?(@node) && args[:include_node]
+	line = options[:before]
+	@page.ancestors.each do |page|
+	  line += link_to(page.menu, root_url+page.url) + options[:separator]
+	end
+	line += link_to(@page.menu,root_url+@page.url) if options[:include_self]
 
-	line += args[:after]
+	if defined?(@node) && options[:include_node]
+	  line += options[:separator] +
+	    options[:before_node] +
+	    content_tag(:span,link_to(@node.title,root_url+@node.url),:class => 'node') +
+	    options[:after_node]
+	end
 
-	return line
+	line += options[:after]
+
+	content_tag options[:framing_tag], line, options[:html]
+      # if no @page specified:
       rescue
 	''
       end
 
       # UL LI navigation menu
-      def ul_li_menu(name='main',args={})
+      def ul_li_menu(name='main',options={})
 	default_options = {
-	  :roots_only => false
-	}
-	default_html_options = {
-	  :id => 'nav',
-	  :class =>'',
+	  :roots_only => false,
 	  :dir_class => 'dir',
 	  :active_class => 'active',
 	  :active_link_class => 'active'
 	}
+	default_html_options = { :id => 'nav' }
 
-	if args[:html]
-	  args[:html] = default_html_options.merge(args[:html])
+	if options[:html]
+	  options[:html] = default_html_options.merge(options[:html])
 	else
-	  args[:html] = default_html_options
+	  options[:html] = default_html_options
 	end
-	args = default_options.merge(args)
+	options = default_options.merge(options)
 
 	view = ''
 	Page.roots.each do |root|
@@ -59,13 +75,13 @@ module JJane
 	  'shared/ul_li_menu',
 	  :page => root,
 	  :menu_name => name,
-	  :roots_only => args[:roots_only],
-	  :dir_class => args[:html][:dir_class],
-	  :active_class => args[:html][:active_class],
-	  :active_link_class => args[:html][:active_link_class]
+	  :roots_only => options[:roots_only],
+	  :dir_class => options[:dir_class],
+	  :active_class => options[:active_class],
+	  :active_link_class => options[:active_link_class]
 	  )
 	end
-	content_tag :ul, view, :id => args[:html][:id], :class => args[:html][:class]
+	content_tag :ul, view, options[:html]
       end
 
     end
