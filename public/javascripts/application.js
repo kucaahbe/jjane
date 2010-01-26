de94f88a5f1a88656a6a29755b533f95a249e919
kucaahbe <powta@ukr.net>
2010-01-27 01:02:16 +0200

    [TMP]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,60 +21,70 @@ function checkValueNotEmpty(button_id) {
 //  $(button_id).disabled = false;
 }
 
+function updateContent(content_id,request) {
+  new Ajax.Updater(content_id, request, {
+      method:'get',
+      onSuccess: function(transport){
+      var response = transport.responseText || "no response text";
+      alert("Success! \n\n" + response);
+      }, onFailure: function(){ alert('Something went wrong...') }
+      });
+}
+
 /*-----------------------------------------------------------
-    Toggles element's display value
-    Input: any number of element id's
-    Output: none 
-    ---------------------------------------------------------*/
+  Toggles element's display value
+Input: any number of element id's
+Output: none 
+---------------------------------------------------------*/
 function toggleDisp() {
-    for (var i=0;i<arguments.length;i++){
-        var d = $(arguments[i]);
-        if (d.style.display == 'none')
-            d.style.display = 'block';
-        else
-            d.style.display = 'none';
-    }
+  for (var i=0;i<arguments.length;i++){
+    var d = $(arguments[i]);
+    if (d.style.display == 'none')
+      d.style.display = 'block';
+    else
+      d.style.display = 'none';
+  }
 }
 /*-----------------------------------------------------------
-    Toggles tabs - Closes any open tabs, and then opens current tab
-    Input:     1.The number of the current tab
-                    2.The number of tabs
-                    3.(optional)The number of the tab to leave open
-                    4.(optional)Pass in true or false whether or not to animate the open/close of the tabs
-    Output: none 
-    ---------------------------------------------------------*/
+  Toggles tabs - Closes any open tabs, and then opens current tab
+Input:     1.The number of the current tab
+2.The number of tabs
+3.(optional)The number of the tab to leave open
+4.(optional)Pass in true or false whether or not to animate the open/close of the tabs
+Output: none 
+---------------------------------------------------------*/
 function toggleTab(num,numelems,opennum,animate) {
-    if ($('tabContent'+num).style.display == 'none'){
-        for (var i=1;i<=numelems;i++){
-            if ((opennum == null) || (opennum != i)){
-                var temph = 'tabHeader'+i;
-                var h = $(temph);
-                if (!h){
-                    var h = $('tabHeaderActive');
-                    h.id = temph;
-                }
-                var tempc = 'tabContent'+i;
-                var c = $(tempc);
-                if(c.style.display != 'none'){
-                    if (animate || typeof animate == 'undefined')
-                        Effect.toggle(tempc,'blind',{duration:0.5, queue:{scope:'menus', limit: 3}});
-                    else
-                        toggleDisp(tempc);
-                }
-            }
-        }
-        var h = $('tabHeader'+num);
-        if (h)
-            h.id = 'tabHeaderActive';
-        h.blur();
-        var c = $('tabContent'+num);
-        c.style.marginTop = '2px';
-        if (animate || typeof animate == 'undefined'){
-            Effect.toggle('tabContent'+num,'blind',{duration:0.5, queue:{scope:'menus', position:'end', limit: 3}});
-        }else{
-            toggleDisp('tabContent'+num);
-        }
+  if ($('tabContent'+num).style.display == 'none'){
+    for (var i=1;i<=numelems;i++){
+      if ((opennum == null) || (opennum != i)){
+	var temph = 'tabHeader'+i;
+	var h = $(temph);
+	if (!h){
+	  var h = $('tabHeaderActive');
+	  h.id = temph;
+	}
+	var tempc = 'tabContent'+i;
+	var c = $(tempc);
+	if(c.style.display != 'none'){
+	  if (animate || typeof animate == 'undefined')
+	    Effect.toggle(tempc,'blind',{duration:0.5, queue:{scope:'menus', limit: 3}});
+	  else
+	    toggleDisp(tempc);
+	}
+      }
     }
+    var h = $('tabHeader'+num);
+    if (h)
+      h.id = 'tabHeaderActive';
+    h.blur();
+    var c = $('tabContent'+num);
+    c.style.marginTop = '2px';
+    if (animate || typeof animate == 'undefined'){
+      Effect.toggle('tabContent'+num,'blind',{duration:0.5, queue:{scope:'menus', position:'end', limit: 3}});
+    }else{
+      toggleDisp('tabContent'+num);
+    }
+  }
 }
 
 
@@ -99,16 +109,16 @@ function jjaneRubyCommander (field_name, url, type, win) {
   }
 
   tinyMCE.activeEditor.windowManager.open( {
-      file : cmsURL,
-      title : 'JJane file browser',
-      width : 800,
-      height : 600,
-      resizable : "yes",
-      inline : "yes",  // This parameter only has an effect if you use the inlinepopups plugin!
-      close_previous : "no"
-      }, {
-      window : win,
-      input : field_name
-      } );
-  return false;
+file : cmsURL,
+title : 'JJane file browser',
+width : 800,
+height : 600,
+resizable : "yes",
+inline : "yes",  // This parameter only has an effect if you use the inlinepopups plugin!
+close_previous : "no"
+}, {
+window : win,
+input : field_name
+} );
+return false;
 }
