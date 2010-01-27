381e545779c9970f7340a6af06c530b89b188d1b
kucaahbe <powta@ukr.net>
2010-01-27 12:18:39 +0200

    some usability fixes

--- lib/jjane/models/attached_file.rb
+++ lib/jjane/models/attached_file.rb
@@ -7,10 +7,9 @@ class AttachedFile < ActiveRecord::Base
 
   has_attached_file :atachment, :styles => { :preview => "100x100>" }
 
+  before_validation :set_name
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :directory_id
-
-  before_validation :set_name
 
   def file?
     self.atachment_file_name
@@ -25,7 +24,7 @@ class AttachedFile < ActiveRecord::Base
   end
 
   def extension
-    self.atachment.content_type.slice(EXT_REGEXP) if self.file?
+    self.atachment.content_type.slice(EXT_REGEXP) if self.file?#BUGHERE
   end
 
   def <=>(another)
@@ -40,8 +39,8 @@ class AttachedFile < ActiveRecord::Base
   private
 
   def set_name
-    unless self.directory?
-      self.name = self.atachment_file_name
+    if self.file? and self.new_record?
+      self.name = self.atachment_file_name.gsub(/\.\w+$/,'')
       self.name = Time.now.to_s(:number)+'_'+self.name if self.class.find_by_name_and_directory_id(self.name,self.directory_id)
     end
   end
