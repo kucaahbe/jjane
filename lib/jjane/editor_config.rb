ff389edec90a4dd281b986624b2166e9a79e1000
kucaahbe <powta@ukr.net>
2010-02-06 02:18:07 +0200

    tiny mce fix

--- lib/jjane/editor_config.rb
+++ lib/jjane/editor_config.rb
@@ -1,7 +1,8 @@
 class TinyMCEconfig
   DEFAULT_SETTINGS = {
     :file_browser_callback => 'jjaneRubyCommander',
-    :relative_urls => false
+    :relative_urls => false,
+    :doctype => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
   }
   def self.load(config='default')
     config = File.open(File.join(RAILS_ROOT, 'config', "tiny_mce_config_#{config}.yml"))
