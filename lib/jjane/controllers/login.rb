13d5be1d52a7fe2d9065c2e8339875e7920eff38
kucaahbe <powta@ukr.net>
2010-04-14 14:56:20 +0300

    back_url feature [issue 181 closed]

--- lib/jjane/controllers/login.rb
+++ lib/jjane/controllers/login.rb
@@ -6,23 +6,23 @@ class LoginController < ApplicationController
     error_404 if logged_in?
   end
 
-  def error(redirect_place = :root_url)#TODO redirect # for title 'Login::Error'
+  def error# for title Login:Error
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:login_notice] = t(:notice_logged_in)
-      redirect_to method(redirect_place).call
+      redirect_to back_url
     else
       flash[:login_error] = t(:notice_invalid_login_or_password)
       render :action => 'welcome'
     end
   end
 
-  def logout(redirect_place = :root_url)#TODO redirect
+  def logout
     if current_user_session then
       @user_session = UserSession.find(params[:id])
       @user_session.destroy
       flash[:login_notice] = t(:notice_logged_out)
-      redirect_to method(redirect_place).call
+      redirect_to back_url
     else
       error_404
     end
@@ -32,5 +32,13 @@ class LoginController < ApplicationController
 
   def check_session
     error_404 if current_user_session
+  end
+
+  def back_url
+    if params[:back_url]
+      params[:back_url]
+    else
+      root_url
+    end
   end
 end
--- lib/jjane/helpers/user_session_helper.rb
+++ lib/jjane/helpers/user_session_helper.rb
@@ -3,6 +3,20 @@ class JJane
     # Methods of this helper can be used in views and controllers
     module User_SessionHelper
 
+      #--
+      #TODO переместить куда-то эти два метода,например в layout_helper
+      #TODO что бы сохраняло back_url при повторном неправильном вводе логина-пароля
+      #++
+      def login_form options = {}, &proc
+	back_url = request.url unless controller_name == 'login'
+	form_for UserSession.new, options.merge(:url => login_path(:back_url => back_url)), &proc
+      end
+
+      def logout_link name, options = {}
+	back_url = request.url unless controller_name == 'login'
+	link_to name, logout_path(:back_url => back_url), options.merge(:method => :post)
+      end
+
       def current_user_session
 	return @current_user_session if defined?(@current_user_session)
 	@current_user_session = UserSession.find
